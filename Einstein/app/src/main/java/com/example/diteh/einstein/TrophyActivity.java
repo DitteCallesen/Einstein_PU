dc812a7f22c20d9128ee587558f831e5b17e3f3d
masolei <maria@soleim.com>
2017-02-28 15:25:00 +0100

    Lagt til metoder for å vise trofeer.

--- Einstein/app/src/main/java/com/example/diteh/einstein/TrophyActivity.java
+++ Einstein/app/src/main/java/com/example/diteh/einstein/TrophyActivity.java
@@ -9,16 +9,79 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrophyActivity extends AppCompatActivity {
+
+    List<Integer> activeTrophies = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trophy);
+
+        showTrophies();
+
     }
+
+    public void addNewTrophy(Integer trophyNumber) {
+        //
+    }
+
+    public void showTrophies() {
+        if (activeTrophies.contains(1)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy1);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(2)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy2);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(3)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy3);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(4)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy4);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(5)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy5);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(6)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy6);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(7)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy7);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(8)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy8);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(9)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy9);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(10)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy10);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(11)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy11);
+            imageButton.setVisibility(View.VISIBLE);
+        }
+        if (activeTrophies.contains(12)) {
+            ImageButton imageButton = (ImageButton) findViewById(R.id.trophy12);
+            imageButton.setVisibility(View.VISIBLE);
+        }
 
 
 
