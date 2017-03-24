302158c77c6a27d65fa0a7735232d1e1d65fdf06
masolei <maria@soleim.com>
2017-03-24 13:57:26 +0100

    Test a button.

---
+++ Einstein/app/src/androidTest/java/com/example/diteh/einstein/MainActivityTest.java
@@ -0,0 +1,61 @@
+package com.example.diteh.einstein;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+/**
+ * Created by mariasoleim on 24.03.2017.
+ */
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class) {
+        @Override
+        protected Intent getActivityIntent() {
+            Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+            Intent result = new Intent(targetContext, MainActivity.class);
+            result.putExtra("Name", "Value");
+            return result;
+        }
+    };
+
+    private MainActivity mActivity = null;
+
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(Class1Activity.class.getName(), null, false);
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunchOfClass1ActivityOnButtonClick() {
+        assertNotNull(mActivity.findViewById(R.id.class1Button));
+        onView(withId(R.id.class1Button)).perform(click());
+        Activity nextActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 5000);
+        assertNotNull(nextActivity);
+        nextActivity.finish();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+
+}
